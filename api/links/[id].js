2616cb9a788f85f75d7e108261ddaad170850417
Developer <user@example.com>
2026-07-03 07:44:51 +0300

    Restore API route functions under Vercel Hobby limit

---
+++ api/links/[id].js
@@ -0,0 +1,17 @@
+const handleAs = require('../../server/vercelHandler');
+
+module.exports = (req, res) => {
+    const id =
+    req.query && req.query.id;
+
+    if (!id) {
+        return res.status(400).json({
+            message: 'Invalid link ID'
+        });
+    }
+
+    req.url =
+    `/api/links/${id}`;
+
+    return handleAs(req.url)(req, res);
+};
---
+++ api/links/search/[text].js
@@ -0,0 +1,17 @@
+const handleAs = require('../../../server/vercelHandler');
+
+module.exports = (req, res) => {
+    const text =
+    req.query && req.query.text;
+
+    if (!text) {
+        return res.status(400).json({
+            message: 'Search text required'
+        });
+    }
+
+    req.url =
+    `/api/links/search/${encodeURIComponent(text)}`;
+
+    return handleAs(req.url)(req, res);
+};
